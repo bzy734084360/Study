9c4ff8098c1ae728771f7f9fc6f9350cb60a96d2
bzy734084360 <bzy734084360@hotmail.com>
2019-11-26 00:12:13 +0800

    XML练习项目

---
+++ _6大项目/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace _6大项目
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgv_user = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.woman = new System.Windows.Forms.RadioButton();
+            this.man = new System.Windows.Forms.RadioButton();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.btnAlter = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_user)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv_user
+            // 
+            this.dgv_user.AllowUserToAddRows = false;
+            this.dgv_user.AllowUserToDeleteRows = false;
+            this.dgv_user.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_user.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgv_user.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgv_user.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgv_user.Location = new System.Drawing.Point(0, 0);
+            this.dgv_user.Name = "dgv_user";
+            this.dgv_user.ReadOnly = true;
+            this.dgv_user.RowHeadersVisible = false;
+            this.dgv_user.RowTemplate.Height = 23;
+            this.dgv_user.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_user.Size = new System.Drawing.Size(405, 228);
+            this.dgv_user.TabIndex = 0;
+            this.dgv_user.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgv_user_CellClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmDelete});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // tsmDelete
+            // 
+            this.tsmDelete.Name = "tsmDelete";
+            this.tsmDelete.Size = new System.Drawing.Size(100, 22);
+            this.tsmDelete.Text = "删除";
+            this.tsmDelete.Click += new System.EventHandler(this.TsmDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名：";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(53, 14);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 21);
+            this.name.TabIndex = 2;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(53, 41);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(46, 21);
+            this.age.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "年龄：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "性别：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAlter);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.woman);
+            this.groupBox1.Controls.Add(this.man);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.id);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.name);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.age);
+            this.groupBox1.Location = new System.Drawing.Point(12, 234);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 115);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "注册";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // woman
+            // 
+            this.woman.AutoSize = true;
+            this.woman.Location = new System.Drawing.Point(310, 21);
+            this.woman.Name = "woman";
+            this.woman.Size = new System.Drawing.Size(35, 16);
+            this.woman.TabIndex = 12;
+            this.woman.Text = "女";
+            this.woman.UseVisualStyleBackColor = true;
+            // 
+            // man
+            // 
+            this.man.AutoSize = true;
+            this.man.Checked = true;
+            this.man.Location = new System.Drawing.Point(259, 21);
+            this.man.Name = "man";
+            this.man.Size = new System.Drawing.Size(35, 16);
+            this.man.TabIndex = 11;
+            this.man.TabStop = true;
+            this.man.Text = "男";
+            this.man.UseVisualStyleBackColor = true;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(259, 47);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 21);
+            this.password.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(212, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "密码：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Id：";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(53, 68);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(35, 21);
+            this.id.TabIndex = 8;
+            // 
+            // btnAlter
+            // 
+            this.btnAlter.Location = new System.Drawing.Point(203, 86);
+            this.btnAlter.Name = "btnAlter";
+            this.btnAlter.Size = new System.Drawing.Size(75, 23);
+            this.btnAlter.TabIndex = 13;
+            this.btnAlter.Text = "修改";
+            this.btnAlter.UseVisualStyleBackColor = true;
+            this.btnAlter.Click += new System.EventHandler(this.BtnAlter_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Id";
+            this.Column1.HeaderText = "id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Name";
+            this.Column2.HeaderText = "姓名";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Gender";
+            this.Column3.HeaderText = "性别";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Age";
+            this.Column4.HeaderText = "年龄";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Password";
+            this.Column5.HeaderText = "密码";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 478);
+            this.Controls.Add(this.dgv_user);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_user)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_user;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.RadioButton woman;
+        private System.Windows.Forms.RadioButton man;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmDelete;
+        private System.Windows.Forms.Button btnAlter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ _6大项目/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6大项目
+{
+    public class User
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        string _id;
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        string _name;
+        /// <summary>
+        /// 密码
+        /// </summary>
+        string _password;
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        string _age;
+        /// <summary>
+        /// 性别
+        /// </summary>
+        string _gender;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Gender { get => _gender; set => _gender = value; }
+        public string Age { get => _age; set => _age = value; }
+        public string Password { get => _password; set => _password = value; }
+    }
+}
