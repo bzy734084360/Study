7343c51fd1bf5c4d6a96f21e8fdd82b46f900a1f
bzy734084360 <bzy734084360@hotmail.com>
2019-11-27 00:15:35 +0800

    第五天视频学习完成

---
+++ _07委托/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07委托
+{
+    /// <summary>
+    /// 数据类型
+    /// </summary>
+    public delegate void MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Show(100);
+            //DoSth("小样");
+            //委托：是数据类型 将方法作为参数进行传递
+            DoSth(Do);
+            Console.ReadKey();
+        }
+
+        public static void DoSth(MyDelegate xx)
+        {
+
+        }
+        public static void Do()
+        {
+
+        }
+
+        //public static void Show(int num)
+        //{
+        //    Console.WriteLine(num + 100);
+        //}
+
+        //public static void DoSth(string name)
+        //{
+        //    Console.WriteLine(name + "好帅");
+        //}
+    }
+    class Person
+    {
+        //字段
+        private string name;
+        //自动属性
+        public string Age { get; set; }
+        public string Name { get => name; set => name = value; }
+    }
+}
